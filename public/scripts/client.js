7d033dbde882544663beed851e49b3be396c214b
Jamaal Ahmed <jamaal729@yahoo.com>
2019-11-06 23:48:12 +0000

    Fixed layout errors on tweets form.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,13 +5,72 @@
  */
 
 $(document).ready(function() {
-  console.log("ready!");
+  console.log("client js ready!");
+
+  $("#tweet-content").html("Tweet content");
+
+  // // Takes a tweet object and returns a tweet <article> element
+  // const createTweetElement = function() {
+  //   //
+  // };
+
+  // // Can be responsible for taking in an array of tweet objects
+  // // and then appending each one to the #tweets-container
+  // // Uses createTweetElement
+  // const renderTweets = function() {
+  //   //
+  // };
+
+  // // Test / driver code (temporary). Eventually will get this from the server.
+  // const tweetData = [
+  //   {
+  //     user: {
+  //       name: "Newton",
+  //       avatars: "https://i.imgur.com/73hZDYK.png",
+  //       handle: "@SirIsaac"
+  //     },
+  //     content: {
+  //       text:
+  //         "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     created_at: 1461116232227
+  //   },
+  //   {
+  //     user: {
+  //       name: "Newton",
+  //       avatars: "https://i.imgur.com/73hZDYK.png",
+  //       handle: "@SirIsaac"
+  //     },
+  //     content: {
+  //       text:
+  //         "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     created_at: 1461116232227
+  //   }
+  // ];
+
+  // const $tweet = createTweetElement(tweetData[0]);
+
+
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac"
+    },
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants"
+    },
+    created_at: 1461116232227
+  };
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
 
-// $(document).ready(() => {
-//   // console.log("client.js ready");
-
-//   document.getElementById("text").addEventListener("click", function() {
-//     console.log("text changing");
-//   });
-// });
