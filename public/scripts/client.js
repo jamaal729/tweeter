2393c51627242302c14ec6145f8937925799b927
Jamaal Ahmed <jamaal729@yahoo.com>
2019-11-07 01:35:47 +0000

    Implemented multiple dynamic tweets.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,20 +7,6 @@
 $(document).ready(function() {
   console.log("client js ready!");
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac"
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1461116232227
-  };
-
   // Takes a tweet object and returns a tweet <article> element
   const createTweetElement = function(obj) {
     console.log(obj.user.name);
@@ -43,11 +29,44 @@ $(document).ready(function() {
     return element;
   };
 
-  const $tweet = createTweetElement(tweetData);
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    for (let tweet of tweets) {
+      let tweetHtml = createTweetElement(tweet);
+      $("#tweets-container").append(tweetHtml);
+    }
+  };
+
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac"
+      },
+      content: {
+        text:
+          "If I have seen further it is by standing on the shoulders of giants"
+      },
+      created_at: 1461116232227
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd"
+      },
+      content: {
+        text: "Je pense , donc je suis"
+      },
+      created_at: 1461113959088
+    }
+  ];
+
+  // renderTweets(data);
+  console.log(renderTweets(data));
 });
-
-// $("#tweet-content").html("Tweet content");
