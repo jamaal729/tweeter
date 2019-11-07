a64c027712bc61578f1f1a4ecf6f0feddda15cc0
Jamaal Ahmed <jamaal729@yahoo.com>
2019-11-07 01:06:35 +0000

    Implemented a single dynamic tweet.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,52 +7,6 @@
 $(document).ready(function() {
   console.log("client js ready!");
 
-  $("#tweet-content").html("Tweet content");
-
-  // // Takes a tweet object and returns a tweet <article> element
-  // const createTweetElement = function() {
-  //   //
-  // };
-
-  // // Can be responsible for taking in an array of tweet objects
-  // // and then appending each one to the #tweets-container
-  // // Uses createTweetElement
-  // const renderTweets = function() {
-  //   //
-  // };
-
-  // // Test / driver code (temporary). Eventually will get this from the server.
-  // const tweetData = [
-  //   {
-  //     user: {
-  //       name: "Newton",
-  //       avatars: "https://i.imgur.com/73hZDYK.png",
-  //       handle: "@SirIsaac"
-  //     },
-  //     content: {
-  //       text:
-  //         "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     created_at: 1461116232227
-  //   },
-  //   {
-  //     user: {
-  //       name: "Newton",
-  //       avatars: "https://i.imgur.com/73hZDYK.png",
-  //       handle: "@SirIsaac"
-  //     },
-  //     content: {
-  //       text:
-  //         "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     created_at: 1461116232227
-  //   }
-  // ];
-
-  // const $tweet = createTweetElement(tweetData[0]);
-
-
-
   // Test / driver code (temporary). Eventually will get this from the server.
   const tweetData = {
     user: {
@@ -67,6 +21,28 @@ $(document).ready(function() {
     created_at: 1461116232227
   };
 
+  // Takes a tweet object and returns a tweet <article> element
+  const createTweetElement = function(obj) {
+    console.log(obj.user.name);
+
+    const element = ` 
+      <article class="tweet">
+        <header>
+          <span>
+            <img id="avatars" src= ${obj.user.avatars} ">
+            <h3 id="name"> ${obj.user.name}</h3>
+          </span>
+          <h3 id="handle"> ${obj.user.handle}</h3>
+        </header>
+        <p id="tweet-content"> ${obj.content.text}</p>
+        <footer>
+          <p>${obj.created_at}</p>
+       </footer>
+      </article>  `;
+
+    return element;
+  };
+
   const $tweet = createTweetElement(tweetData);
 
   // Test / driver code (temporary)
@@ -74,3 +50,4 @@ $(document).ready(function() {
   $("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
 
+// $("#tweet-content").html("Tweet content");
